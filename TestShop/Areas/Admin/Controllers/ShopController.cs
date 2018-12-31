572a6ed367cd23157be93d62ae50f57e25216bd0
Qvein <sylwester_kania@interia.pl>
2018-12-31 15:31:54 +0100

    Shop cz.1

---
+++ TestShop/Areas/Admin/Controllers/ShopController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using TestShop.Models.Data;
+using TestShop.Models.ViewModels.Shop;
+
+namespace TestShop.Areas.Admin.Controllers
+{
+    public class ShopController : Controller
+    {
+        // GET: Admin/Categories
+        public ActionResult Categories()
+        {
+            // deklaracja listy kategorii do wyświetlenia
+            List<CategoryVM> categoryVMList;
+
+            using (Db db = new Db())
+            {
+                categoryVMList = db.Categories.ToArray().OrderBy(x => x.Sorting).Select(x => new CategoryVM(x)).ToList();
+            }
+
+            return View(categoryVMList);
+        }
+    }
+}
