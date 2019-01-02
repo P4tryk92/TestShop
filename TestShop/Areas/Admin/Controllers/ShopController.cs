33bf24ef6272a8d36fd7de5a894e74510617682b
Qvein <sylwester_kania@interia.pl>
2019-01-02 17:27:08 +0100

    Shop cz.3 categories

--- TestShop/Areas/Admin/Controllers/ShopController.cs
+++ TestShop/Areas/Admin/Controllers/ShopController.cs
@@ -9,9 +9,10 @@ namespace TestShop.Areas.Admin.Controllers
     public class ShopController : Controller
     {
         // GET: Admin/Categories
+        [HttpGet]
         public ActionResult Categories()
         {
-            // deklaracja listy kategorii do wyświetlenia
+            // Deklaracja listy kategorii do wyświetlenia
             List<CategoryVM> categoryVMList;
 
             using (Db db = new Db())
@@ -20,6 +21,82 @@ namespace TestShop.Areas.Admin.Controllers
             }
 
             return View(categoryVMList);
+        }
+
+        // POST: Admin/Shop/AddNewCategory
+        [HttpPost]
+        public string AddNewCategory(string catName)
+        {
+            // Deklaracja id
+            string id;
+
+            using (Db db = new Db())
+            {
+                // Sprawdzanie czy nazwa kategorii jest unikalna
+                if (db.Categories.Any(x => x.Name == catName))
+                {
+                    return "tytulzajety";
+                }
+
+                // Inicjalizacja DTO
+                CategoryDTO dto = new CategoryDTO();
+                dto.Name = catName;
+                dto.Slug = catName.Replace(" ", "-").ToLower();
+                dto.Sorting = 1000;
+
+                // Zapis do bazy
+                db.Categories.Add(dto);
+                db.SaveChanges();
+
+                // Pobieramy id
+                id = dto.Id.ToString();
+            }
+            return id;
+        }
+
+        // POST: Admin/Shop/ReorderCategories
+        [HttpPost]
+        public ActionResult ReorderCategories(int[] id)
+        {
+            using (Db db = new Db())
+            {
+                int count = 1;
+
+                // Deklaracja DTO 
+                CategoryDTO dto;
+
+                // Sortowanie kategorii
+                foreach (var catId in id)
+                {
+                    dto = db.Categories.Find(catId);
+                    dto.Sorting = count;
+
+                    db.SaveChanges();
+
+                    count++;
+                }
+            }
+
+            return View();
+        }
+
+        // GET: Admin/Shop/DeleteCategory
+        [HttpGet]
+        public ActionResult DeleteCategory(int id)
+        {
+            using (Db db = new Db())
+            {
+                // pobieramy kategorię o podanym id
+                CategoryDTO dto = db.Categories.Find(id);
+
+                // usuwanie kategorii
+                db.Categories.Remove(dto);
+
+                // zapis
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("Categories");
         }
     }
 }
