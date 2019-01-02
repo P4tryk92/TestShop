475cf2b94ee6f3e3346c5ebf2bdfc7faa46b1dce
Qvein <sylwester_kania@interia.pl>
2019-01-02 22:37:48 +0100

    Shop cz.4 products

--- TestShop/Models/Data/DB.cs
+++ TestShop/Models/Data/DB.cs
@@ -2,10 +2,11 @@
 
 namespace TestShop.Models.Data
 {
-    public class Db :DbContext
+    public class Db : DbContext
     {
         public DbSet<PageDTO> Pages { get; set; }
         public DbSet<SidebarDTO> Sidebar { get; set; }
         public DbSet<CategoryDTO> Categories { get; set; }
+        public DbSet<ProductDTO> Products { get; set; }
     }
 }
